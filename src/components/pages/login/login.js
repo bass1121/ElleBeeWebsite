1e9d7b9a518b09017a7bb1165237cc922be10b4e
Sean Bass <seanbass99@gmail.com>
2021-08-18 12:59:06 -0500

    finished layout for login and created signup logic

--- src/components/pages/login/login.js
+++ src/components/pages/login/login.js
@@ -1,19 +1,117 @@
 import React, { Component } from 'react';
-
-
 class Login extends Component {
-    render() {
-        return (
-            <div className="login-wrapper">
-                <div className="login-title">
-                    Member's Login
-                </div>
-                <div className="login-form">
-                    <input type="email" placeholder="Email" />
-                </div>
-            </div>
-        );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      errorMessage: "",
+      toggleSignup: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.verifyPassword = this.verifyPassword.bind(this);
+    this.handleToggleSignup = this.handleToggleSignup.bind(this);
+  }
+
+  handleToggleSignup(event) {
+    event.preventDefault();
+
+    this.setState(prevState => ({
+      toggleSignup: !prevState.toggleSignup,
+    }));
+  }
+
+  handleChange(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+      errorText: "",
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    if (this.state.toggleSignup) {
+      console.log(
+        this.state.email,
+        this.state.password,
+        this.state.confirmPassword,
+        this.state.errorMessage
+      );
     }
+
+    console.log(this.state.email, this.state.password, this.state.errorMessage);
+  }
+
+  verifyPassword() {
+    return this.state.toggleSignup ? (
+      <div className="login-field">
+        <label for="confirmPassword">Confirm Password:</label>
+        <input
+          id="confirmPassword"
+          type="password"
+          name="confirmPasswword"
+          placeholder="Confirm password"
+          value={this.state.confirmPassword}
+          onChange={this.handleChange}
+          required
+        />
+      </div>
+    ) : null;
+  }
+
+  render() {
+    return (
+      <div className="login-wrapper">
+        <div className="signin-form-title">
+          {this.state.toggleSignup ? "Member's Log In" : "Member's Sign In"}
+        </div>
+        <form onSubmit={this.handleSubmit} className="login-form-wrapper">
+          <div className="login-field">
+            <label for="email">Email:</label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Your email"
+              value={this.state.email}
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+
+          <div className="login-field">
+            <label for="password">Password:</label>
+            <input
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Your password"
+              value={this.state.password}
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+
+          {this.verifyPassword()}
+
+          <div>
+            <button className="login-button" type="submit">
+              {this.state.toggleSignup ? "Member Signup" : "Member Login"}
+            </button>
+            <button className="login-button" onClick={this.handleToggleSignup}>
+              {this.state.toggleSignup
+                ? "Already Signed up?"
+                : "Not Signed Up?"}
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Login;
--- src/components/pages/members.js
+++ src/components/pages/members.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import ElleDrawing from "../../images/Elle-Bee-Drawing.jpg";
 import DarkBee from "../../images/darkslategreyBee.png";
-
+import LoginForm from "../pages/login/login";
 
 export default class Members extends Component {
-    render() {
-        return (
-            <div className="members-wrapper">
-              <div className="member-header">
-                <div className="drawing-wrapper">
-                  <img src={ElleDrawing} alt="" />
-                </div>
-                <div className="member-container-wrapper">
-                  <div className="member-page-title">
-                    Member's Sign In
-                  </div>
-                </div>
-                <div className="dark-bee-wrapper">
-                  <img src={DarkBee} alt="" />
-                </div>
-              </div>
-            </div>
-          );
-    }
+  render() {
+    return (
+      <div className="members-wrapper">
+        <div className="member-header">
+          <div className="drawing-wrapper">
+            <img src={ElleDrawing} alt="" />
+          </div>
+          <div className="member-container-wrapper">
+            <LoginForm />
+          </div>
+          <div className="dark-bee-wrapper">
+            <img src={DarkBee} alt="" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
