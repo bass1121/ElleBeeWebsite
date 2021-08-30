4778d833e5ffd2971e0b4974adfb08fd5abae50a
Sean Bass <seanbass99@gmail.com>
2021-08-30 18:00:54 -0500

    commented out redux store enabler

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,8 +6,8 @@ const store = createStore(
   reducers,
   {},
   compose(
-    applyMiddleware(reduxThunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(reduxThunk)
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
